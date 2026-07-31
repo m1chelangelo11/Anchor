f465b65f5756ff592d4b25ca8726142cd2e09789
m1chelangelo11 <1michalaniol1@gmail.com>
2026-07-31 21:26:11 +0200

    refactor: added docstrings and type hints to model.py

--- src/model.py
+++ src/model.py
@@ -4,14 +4,45 @@ import torch.nn as nn
 
 
 class Predictor(nn.Module):
-    def __init__(self, hidden_size=2):
+    """
+    Class representing a one-step predictor model.
+
+    Attributes:
+        hidden_layer (torch.nn.Linear): Linear hidden layer
+        activation (torch.nn.Tanh): Tanh activation layer
+        output_layer (torch.nn.Linear): Linear output layer
+    """
+
+
+
+    def __init__(self, hidden_size: int = 64):
+        """
+        Initialize Predictor object.
+
+        Args:
+            hidden_size (int): size of the hidden layers, default
+            value is 64 because of experiments
+        """
+
         super().__init__()
 
         self.hidden_layer = nn.Linear(2, hidden_size)
         self.activation = nn.Tanh()
         self.output_layer = nn.Linear(hidden_size, 2)
 
-    def forward(self, x):
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Executes a forward pass through a neural network.
+
+        Args:
+            x (torch.Tensor): input tensor with data package
+        
+        Returns:
+            torch.Tensor: output tensor with model predictions
+        """
+
         x = self.hidden_layer(x)
         x = self.activation(x)
         x = self.output_layer(x)
@@ -19,9 +50,3 @@ class Predictor(nn.Module):
         return x
 
     
-
-if __name__ == "__main__":
-    model = Predictor()
-    x = torch.randn(5, 2)
-    out = model(x)
-    print(out.shape)
